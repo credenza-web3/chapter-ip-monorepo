c2f706416e673c5dd6dc12a797760180b3818de1
Evgen Pluzhnikov <eugene.pluzhnikov@gmail.com>
2025-12-03 14:39:50 +0200

    price for upload page

--- apps/admin/src/routes/authed/upload/+page.svelte
+++ apps/admin/src/routes/authed/upload/+page.svelte
@@ -9,6 +9,25 @@
   let loading = $state(false)
   let fileInput: HTMLInputElement | null = $state(null)
   let uploaded: File | null = $state(null)
+  let isLifetimeLicense = $state(false)
+  let isOneTimeLicense = $state(false)
+  let lifetimePrice = $state(0)
+  let oneTimePrice = $state(0)
+  let isFormValid = $state(false)
+
+  $effect(() => {
+    if (!isLifetimeLicense) lifetimePrice = 0
+    if (!isOneTimeLicense) oneTimePrice = 0
+    isFormValid = (isLifetimeLicense && lifetimePrice > 0) || (isOneTimeLicense && oneTimePrice > 0)
+    console.log(isFormValid)
+  })
+
+  function resetCheckboxes() {
+    isLifetimeLicense = false
+    isOneTimeLicense = false
+    lifetimePrice = 0
+    oneTimePrice = 0
+  }
 
   beforeNavigate(() => {
     loading = true
@@ -38,10 +57,12 @@
     }
 
     uploaded = file
+    resetCheckboxes()
   }
 
   const onClear = () => {
     uploaded = null
+    resetCheckboxes()
     if (fileInput) fileInput.value = ''
   }
 
@@ -93,7 +114,10 @@
   }
 </script>
 
-<div class="mx-10">
+<div class="p-4 min-h-xl">
+  <div class="mb-12 text-center">
+    <h1 class="text-4xl font-light text-gray-900">Upload File</h1>
+  </div>
   {#if loading}
     <div class="flex items-center justify-center h-16">
       <span class="loading loading-dots loading-lg"></span>
@@ -139,8 +163,49 @@
       <input type="file" class="hidden" bind:this={fileInput} onchange={handleInput} />
     </div>
     {#if uploaded}
-      <div class="flex gap-10 mt-3">
-        <button class="btn btn-outline" onclick={onSubmitClick} disabled={loading}>Upload</button>
+      <div class="mt-6 space-y-4">
+        <fieldset class="fieldset bg-base-100 rounded-box border p-4 max-w-lg">
+          <legend class="fieldset-legend">Choose License Type</legend>
+          <label class="label justify-between">
+            <div class="space-x-2">
+              <input type="checkbox" bind:checked={isLifetimeLicense} class="checkbox" />
+              <span class="label-text">Lifetime License</span>
+            </div>
+            {#if isLifetimeLicense}
+              <input
+                type="number"
+                class="input validator max-w-xs"
+                required
+                placeholder="Enter the price in dollars."
+                min="1"
+                bind:value={lifetimePrice}
+              />
+            {/if}
+          </label>
+          <label class="label justify-between">
+            <div class="space-x-2">
+              <input type="checkbox" bind:checked={isOneTimeLicense} class="checkbox" />
+              <span class="label-text">One Time License</span>
+            </div>
+
+            {#if isOneTimeLicense}
+              <input
+                type="number"
+                class="input validator max-w-xs"
+                required
+                placeholder="Enter the price in dollars."
+                min="1"
+                bind:value={oneTimePrice}
+              />
+            {/if}
+          </label>
+        </fieldset>
+
+        <div class="flex gap-10 mt-3">
+          <button class="btn btn-outline" onclick={onSubmitClick} disabled={loading} class:hidden={!isFormValid}
+            >Upload</button
+          >
+        </div>
       </div>
     {/if}
   {/if}
