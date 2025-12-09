dd0cfec4f60752899d93f8f6e42352de3f3d8892
mavvad <mavvad00@gmail.com>
2025-12-09 11:54:53 +0100

    ui loaders

--- apps/frontend/src/routes/authed/+layout.svelte
+++ apps/frontend/src/routes/authed/+layout.svelte
@@ -2,12 +2,23 @@
   import { passportStore } from '$lib/passport.store'
   import { Passport } from '@credenza3/passport-evm'
   import { onMount } from 'svelte'
+  import { afterNavigate, beforeNavigate } from '$app/navigation'
 
   let { children } = $props()
 
   let isMounted = $state(false)
+  let loading = $state(false)
+
+  beforeNavigate(() => {
+    loading = true
+  })
+
+  afterNavigate(() => {
+    loading = false
+  })
 
   onMount(async () => {
+    loading = true
     const passport = new Passport({
       chainId: '43113',
       clientId: import.meta.env.VITE_CLIENT_ID,
@@ -22,9 +33,14 @@
     await passport.init()
     passportStore.set(passport)
     isMounted = true
+    loading = false
   })
 </script>
 
-{#if isMounted}
+{#if loading}
+  <div class="flex items-center justify-center h-16">
+    <span class="loading loading-dots loading-lg"></span>
+  </div>
+{:else if isMounted}
   {@render children?.()}
 {/if}
