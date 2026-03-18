77a6a682d3ad383f453e6a6de75746bea31fb070
dfokin <dmitry.fokin@macrodemic.com>
2026-03-18 13:05:08 +0200

    fix

--- apps/backend/src/file/file.service.ts
+++ apps/backend/src/file/file.service.ts
@@ -63,7 +63,7 @@ export class FileService extends CommonModelService<File> {
     return result
   }
 
-  getBucketName(type: 'metadata' | 'content' | 'preview') {
+  getBucketName(type: 'metadata' | 'content' | 'preview' | 'images') {
     let bucketName = `chapter-ip-${type}`
     const env = this.configService.get<string>('NODE_ENV')
     if (env !== 'prod') {
