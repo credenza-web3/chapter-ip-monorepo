17b66ac3a946e06a8936b5783e0fe88ec156a351
Evgen Pluzhnikov <eugene.pluzhnikov@gmail.com>
2026-05-25 18:24:24 +0300

    close modal

--- packages/ui/components/modals/Base.svelte
+++ packages/ui/components/modals/Base.svelte
@@ -8,11 +8,11 @@
     class="flex flex-col max-w-[900px] relative bg-[#f5f1ec] rounded-[12px] w-full overflow-y-scroll h-[350px] max-h-screen px-5 py-4"
   >
     {#if isBack}
-      <div class="flex align-center justify-start text-xs text-[#70707080] gap-2 cursor-pointer" onclick={onClose}>
+      <button type="button" class="flex align-center justify-start text-xs text-[#70707080] gap-2 cursor-pointer" onclick={onClose}>
         <img src={ArrowDown} class="w-3 h-3 rotate-90" alt="" /> Back
-      </div>
+      </button>
     {/if}
-    <button type="button" class="text-[20px] text-[#70707080] rounded-none absolute right-3 top-2" onclick={onClose}
+    <button type="button" class="text-[20px] text-[#70707080] rounded-none absolute right-3 top-2 cursor-pointer" onclick={onClose}
       >✕</button
     >
     <div class="px-6 py-7">
