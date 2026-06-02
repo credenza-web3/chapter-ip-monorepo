b56871db019d4ef2ec60028e4953783abaa736e3
dfokin <dmitry.fokin@macrodemic.com>
2026-06-02 15:05:23 +0300

    fix

--- packages/notifications/src/index.ts
+++ packages/notifications/src/index.ts
@@ -9,8 +9,8 @@ export const NOTIFICATION_TYPE = {
   LOCATION_PURCHASED: 'LOCATION_PURCHASED',
   OTHER: 'OTHER',
 } as const
-export type TNotificationType = typeof NOTIFICATION_TYPE[keyof typeof NOTIFICATION_TYPE]
-export const NOTIFICATION_TYPE_VALUES = Object.values(NOTIFICATION_TYPE);
+export type TNotificationType = (typeof NOTIFICATION_TYPE)[keyof typeof NOTIFICATION_TYPE]
+export const NOTIFICATION_TYPE_VALUES = Object.values(NOTIFICATION_TYPE)
 
 export const notificationSchema = z.object({
   type: z.enum(NOTIFICATION_TYPE_VALUES),
