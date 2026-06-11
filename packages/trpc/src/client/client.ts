423a65ee3e2cd337d5bcfb6a3bdcc64fffbef591
Evgen Pluzhnikov <eugene.pluzhnikov@gmail.com>
2026-06-11 10:44:35 +0300

    Enable real-time notifications with TRPC and auth

--- packages/trpc/src/client/client.ts
+++ packages/trpc/src/client/client.ts
@@ -1,29 +1,74 @@
-import { createTRPCProxyClient, httpBatchLink, type TRPCClient } from '@trpc/client'
-import type { AppRouter } from '../server/server' // e.g. copied from backend
+// import { createTRPCProxyClient, httpBatchLink, type TRPCClient } from '@trpc/client'
+// import type { AppRouter } from '../server/server' // e.g. copied from backend
 
-export { type TRPCClient } from '@trpc/client'
-export { type AppRouter } from '../server/server'
+// export { type TRPCClient } from '@trpc/client'
+// export { type AppRouter } from '../server/server'
+
+// export function createClient(opts: {
+//   trpcUrl?: string
+//   headers?: Record<string, string>
+//   getAccessTokenFn?: () => string
+// }): TRPCClient<AppRouter> {
+//   opts.trpcUrl = opts.trpcUrl || 'http://localhost:8060/trpc'
+
+//   return createTRPCProxyClient<AppRouter>({
+//     links: [
+//       httpBatchLink({
+//         url: opts.trpcUrl,
+//         headers: () => {
+//           const headers: Record<string, string> = opts.headers || {}
+//           if (opts.getAccessTokenFn && typeof opts.getAccessTokenFn === 'function') {
+//             const accessToken = opts.getAccessTokenFn()
+//             headers.Authorization = `Bearer ${accessToken}`
+//           }
+
+//           return headers
+//         },
+//       }),
+//     ],
+//   })
+// }
+
+import {
+  createTRPCProxyClient,
+  httpBatchLink,
+  httpSubscriptionLink,
+  splitLink
+} from '@trpc/client'
+import type { AppRouter } from '../server/server'
 
 export function createClient(opts: {
   trpcUrl?: string
   headers?: Record<string, string>
-  getAccessTokenFn?: () => string
-}): TRPCClient<AppRouter> {
-  opts.trpcUrl = opts.trpcUrl || 'http://localhost:8060/trpc'
+  getAccessTokenFn?: () => string | Promise<string>
+}) {
+  opts.trpcUrl = opts.trpcUrl || 'http://localhost:5001/trpc'
 
   return createTRPCProxyClient<AppRouter>({
     links: [
-      httpBatchLink({
-        url: opts.trpcUrl,
-        headers: () => {
-          const headers: Record<string, string> = opts.headers || {}
-          if (opts.getAccessTokenFn && typeof opts.getAccessTokenFn === 'function') {
-            const accessToken = opts.getAccessTokenFn()
-            headers.Authorization = `Bearer ${accessToken}`
+      splitLink({
+        condition: (op) => op.type === 'subscription',
+        true: httpSubscriptionLink({
+          url: opts.trpcUrl,
+          connectionParams: async () => {
+            if (opts.getAccessTokenFn && typeof opts.getAccessTokenFn === 'function') {
+              const accessToken = await opts.getAccessTokenFn()
+              return { Authorization: `Bearer ${accessToken}` }
+            }
+            return { Authorization: undefined } 
           }
-
-          return headers
-        },
+        }),
+        false: httpBatchLink({
+          url: opts.trpcUrl,
+          headers: async () => { 
+            const headers: Record<string, string> = opts.headers || {}
+            if (opts.getAccessTokenFn && typeof opts.getAccessTokenFn === 'function') {
+              const accessToken = await opts.getAccessTokenFn()
+              headers.Authorization = `Bearer ${accessToken}`
+            }
+            return headers
+          }
+        }),
       }),
     ],
   })
