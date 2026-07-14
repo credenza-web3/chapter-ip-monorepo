28f49b40cfef2ec608e4236034c6bda1cb0ad3f0
fokindmitr <fokindmitr@gmail.com>
2026-07-14 16:47:21 +0300

    fix userfiles bucket url location

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -8,10 +8,12 @@ export const r2BaseConfig =
     ? {
         previewUrl: 'https://pub-b3f1a770bd1e4f3d8a38569d214475c8.r2.dev',
         defaultImageUrl: 'https://pub-1a5fde2f5a814d7bbcaca6562a705028.r2.dev/chapter_ip.png',
+        userFilesUrl: 'https://pub-4f961b3278ea4bdc98b0cb5a715ec021.r2.dev',
       }
     : {
         previewUrl: 'https://pub-1a5fde2f5a814d7bbcaca6562a705028.r2.dev',
         defaultImageUrl: 'https://pub-1a5fde2f5a814d7bbcaca6562a705028.r2.dev/chapter_ip.png',
+        userFilesUrl: 'https://pub-04dcdc21f78148809433f5a566589248.r2.dev',
       }
 
 // export const r2Config = {
