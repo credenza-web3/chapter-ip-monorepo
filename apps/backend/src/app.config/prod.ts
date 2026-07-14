486792517c8522b23b8ea7d48cbdcc85dfce9b31
fokindmitr <fokindmitr@gmail.com>
2026-07-14 12:19:24 +0300

    Change primary Avalanche WebSocket to drpc.org

--- apps/backend/src/app.config/prod.ts
+++ apps/backend/src/app.config/prod.ts
@@ -7,7 +7,7 @@ export default () => ({
   },
   evm: {
     rpcUrl: 'https://api.avax.network/ext/bc/C/rpc',
-    wsUrls: ['wss://api.avax.network/ext/bc/C/ws', 'wss://avalanche-c-chain-rpc.publicnode.com'],
+    wsUrls: ['wss://avalanche.drpc.org', 'wss://avalanche-c-chain-rpc.publicnode.com'],
     contentNftContractAddress: '0x677c95a69ACf072F7e5af943aD3561feE0dd166b',
     licenseNftContractAddress: '0xeb85E9a26e626389640be966fB62Ad1b70fC45a3',
     membershipContractAddress: '0x031d3E3D026480C938cC4AC4605EcCc57910F5CB',
